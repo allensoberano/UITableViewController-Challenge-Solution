1a2d80320d72df495844b18969d1a63eb77bad4e
Allen Soberano <Allen@Allens-MacBook-Pro.local>
2014-04-29 14:58:09 -0700

    solved the challenge

---
+++ UITableViewController Challenge Solution/ASTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ASTableViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Allen Soberano on 4/29/14.
+//  Copyright (c) 2014 Allen Soberano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ASTableViewController : UITableViewController
+
+@end
